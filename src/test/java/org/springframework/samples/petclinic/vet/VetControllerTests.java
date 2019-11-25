4946f97c3ac16c40739d39445d66047ed5fca43d
Mateus Gautério <50017946+MateusABG@users.noreply.github.com>
2019-11-25 13:07:36 -0300

    Update VetControllerTests.java

--- src/test/java/org/springframework/samples/petclinic/vet/VetControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/vet/VetControllerTests.java
@@ -14,72 +14,73 @@
  * limitations under the License.
  */
 
-package org.springframework.samples.petclinic.vet;
+package org.springframework.samples.petclinic.owner;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import org.assertj.core.util.Lists;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.samples.petclinic.visit.VisitRepository;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 /**
- * Test class for the {@link VetController}
+ * Test class for {@link VisitController}
+ *
+ * @author Colin But
  */
-@RunWith(SpringRunner.class)
-@WebMvcTest(VetController.class)
-public class VetControllerTests {
+@WebMvcTest(VisitController.class)
+class VisitControllerTests {
+
+    private static final int TEST_PET_ID = 1;
 
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
-    private VetRepository vets;
+    private VisitRepository visits;
 
-    @Before
-    public void setup() {
-        Vet james = new Vet();
-        james.setFirstName("James");
-        james.setLastName("Carter");
-        james.setId(1);
-        Vet helen = new Vet();
-        helen.setFirstName("Helen");
-        helen.setLastName("Leary");
-        helen.setId(2);
-        Specialty radiology = new Specialty();
-        radiology.setId(1);
-        radiology.setName("radiology");
-        helen.addSpecialty(radiology);
-        given(this.vets.findAll()).willReturn(Lists.newArrayList(james, helen));
+    @MockBean
+    private PetRepository pets;
+
+    @BeforeEach
+    void init() {
+        given(this.pets.findById(TEST_PET_ID)).willReturn(new Pet());
     }
 
     @Test
-    public void testShowVetListHtml() throws Exception {
-        mockMvc.perform(get("/vets.html"))
+    void testInitNewVisitForm() throws Exception {
+        mockMvc.perform(get("/owners/*/pets/{petId}/visits/new", TEST_PET_ID))
             .andExpect(status().isOk())
-            .andExpect(model().attributeExists("vets"))
-            .andExpect(view().name("vets/vetList"));
+            .andExpect(view().name("pets/createOrUpdateVisitForm"));
     }
 
     @Test
-    public void testShowResourcesVetList() throws Exception {
-        ResultActions actions = mockMvc.perform(get("/vets")
-            .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
-        actions.andExpect(content().contentType("application/json;charset=UTF-8"))
-            .andExpect(jsonPath("$.vetList[0].id").value(1));
+    void testProcessNewVisitFormSuccess() throws Exception {
+        mockMvc.perform(post("/owners/*/pets/{petId}/visits/new", TEST_PET_ID)
+            .param("name", "George")
+            .param("description", "Visit Description")
+            .param("prescription", "Visit Prescription")
+        )
+            .andExpect(status().is3xxRedirection())
+            .andExpect(view().name("redirect:/owners/{ownerId}"));
+    }
+
+    @Test
+    void testProcessNewVisitFormHasErrors() throws Exception {
+        mockMvc.perform(post("/owners/*/pets/{petId}/visits/new", TEST_PET_ID)
+            .param("name", "George")
+        )
+            .andExpect(model().attributeHasErrors("visit"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("pets/createOrUpdateVisitForm"));
     }
 
 }
